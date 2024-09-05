bad78996096a0f67988cf3b97ce61139a6dc521c
Liza <lizaapostol378@gmail.com>
2024-09-05 22:33:24 +0300

    update js

--- js/modal.js
+++ js/modal.js
@@ -41,6 +41,35 @@
   });
 })();
 
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+  }
+})();
+
 document.addEventListener("DOMContentLoaded", function () {
   // Отключаем автоматическое восстановление прокрутки
   if ("scrollRestoration" in window.history) {
